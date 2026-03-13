13ff44b03f4210d3a8e36f91d3af952b7838e1a5
Hannes Albert <hannes10042004@icloud.com>
2026-03-13 23:36:47 +0100

    feat: Finish implementation of logic

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,37 @@
 module Main where
 import Graphics.Gloss
 
+deg2rad :: Float -> Float
+deg2rad a = a * pi / 180
+
+get_end :: Point -> Float -> Float -> Point
+get_end (start_x, start_y) len angle = (start_x+(sin (deg2rad angle))*len, start_y+(cos (deg2rad angle))*len) 
+
+
+
 -- start (x, y), length, angle, thickness
 draw_branches :: Point -> Float -> Float -> Float -> Picture
-draw_branches point len angle thickness
+draw_branches (x, y) len angle thickness
   | len > 1 =
       pictures
-        [ drawRect point len angle thickness
---        , draw_branches (x2, y2) (x2-3, y2-5)
+        [ drawRect (x, y) len angle thickness
+        , draw_branches (get_end (x, y) len angle) (0.7*len) (angle+20) (0.7*thickness)
+        , draw_branches (get_end (x, y) len angle) (0.7*len) (angle-20) (0.7*thickness)        
         ]
   | otherwise = Blank
   where
 
-    get_distance :: Point -> Point -> Float
-    get_distance (x1,y1) (x2,y2) =
-      sqrt ((x2-x1)^2 + (y2-y1)^2)
-
-    get_end :: Point -> Float -> Float -> Point
-    get_end (x, y) len angle = (x+(sin angle)*len,y+(cos angle)) 
      
     drawRect :: Point -> Float -> Float -> Float -> Picture
     drawRect (x1,y1) len angle thickness =
-      rotate angle (translate x1 y1 (rectangleSolid thickness len))
+      translate x1 y1
+      (rotate angle
+       (translate 0 (len/2)
+         (rectangleSolid thickness len)))
 
 main :: IO ()
 main =
   display
-    (InWindow "Nice Window" (600,400) (10,10))
+    (InWindow "Fractal trees" (900,600) (10,10))
     (makeColorI 245 245 245 255)
-    (draw_branches (0, (-200)) 60 0 20)
+    (draw_branches (0, (-200)) 200 0 20)
